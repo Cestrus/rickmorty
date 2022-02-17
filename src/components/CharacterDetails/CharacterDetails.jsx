3de4835d2c90d74c4c99f691db32b2a72e274903
Alexey Savchenko <wasxela@gmail.com>
2022-02-17 09:44:07 +0200

    create details styles

--- src/components/CharacterDetails/CharacterDetails.jsx
+++ src/components/CharacterDetails/CharacterDetails.jsx
@@ -2,32 +2,46 @@ import SimpleDiscr from '../SimpleDiscription/SimpleDiscription';
 import styles from './CharacterDetails.module.css';
 
 const CharacterDetails = ({ character }) => {
-  const { name, type, gender, location, image, status, episodes, created } =
+  const { name, type, gender, location, image, status, episode, created } =
     character;
+  const editEpisodes = () => {
+    // console.log('==> ', character);
+    const episodesList = episode.map((epHttp) => {
+      return epHttp.match(/\d+/);
+    });
+    return episodesList.join(', ');
+  };
+
   return (
     <div className={styles.container}>
-      <div className="name">
-        <p>{name}</p>
-      </div>
-      <div className="img-wrapper">
+      <p className={styles.title}>DETAILS</p>
+      <div className={styles['img-wrapper']}>
         <img src={image} alt="" />
       </div>
-      <div className="left-side">
+      <div className={styles.upper}>
+        <SimpleDiscr className={styles.name} title="Name" text={name} />
+        <SimpleDiscr className={styles.gender} title="Gender" text={gender} />
+        <SimpleDiscr className={styles.status} title="Status" text={status} />
+      </div>
+      <div className={styles.medium}>
         <SimpleDiscr
           className={styles.location}
           title="Location"
           text={location.name}
         />
-        <SimpleDiscr className={styles.status} title="Status" text={status} />
+        <SimpleDiscr className={styles.type} title="Type" text={type} />
         <SimpleDiscr
           className={styles.created}
           title="Created"
           text={new Date(created).toLocaleDateString()}
         />
       </div>
-      <div className="right-side">
-        <SimpleDiscr className={styles.type} title="Type" text={type} />
-        <SimpleDiscr className={styles.gender} title="Gender" text={gender} />
+      <div className={styles.bottom}>
+        <SimpleDiscr
+          className={styles.episodes}
+          title="Episodes"
+          text={editEpisodes()}
+        />
       </div>
     </div>
   );
